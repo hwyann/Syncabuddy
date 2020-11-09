9d68676e79fecc44a00287ba79352ff415a8d740
Tomo Norman <tomo.l.norman@gmail.com>
2020-11-09 17:39:50 +0900

    associations created on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
+
+  has_many :attendances
+  has_many :matches, through: :attendances
+  has_many :matches
+  has_many :attendances_as_owner, through: :matches, source: :attendances
 end
