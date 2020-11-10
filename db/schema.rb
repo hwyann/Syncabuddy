c2557b05bb930347f29645c322d8aa54a28cb791
Tomo Norman <tomo.l.norman@gmail.com>
2020-11-10 10:34:08 +0900

    schema update?

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_073955) do
+ActiveRecord::Schema.define(version: 2020_11_09_085500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendances", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "match_id", null: false
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["match_id"], name: "index_attendances_on_match_id"
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
+
+  create_table "matches", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "date"
+    t.text "description"
+    t.string "location"
+    t.string "sport"
+    t.integer "min_attendent"
+    t.integer "max_attendent"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_matches_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +51,7 @@ ActiveRecord::Schema.define(version: 2020_11_09_073955) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "attendances", "matches"
+  add_foreign_key "attendances", "users"
+  add_foreign_key "matches", "users"
 end
