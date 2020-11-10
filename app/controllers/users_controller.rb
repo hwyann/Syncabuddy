1f9c3e13e21b8b154176879085b31eb5342e8ecd
Takuhei Mizutani <takuheim1011@gmail.com>
2020-11-10 14:28:19 +0900

    sort of finished the user dashboard + created attendances controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   # skip_before_action :authenticate_user!, only: :show
 
   def show
-    raise
     @user = User.find(params[:id])
     authorize @user
   end
